82a9011e6f82026f140c3ef117d83bd04593c36c
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-02 03:11:44 -0300

    aula02

--- aula02/aula02.py
+++ aula02/aula02.py
@@ -1,5 +1,5 @@
 # Esse é o modo de criação de comentários no python
-#print(123456) # Isso exibe uma informação no terminal
+# print(123456) # Isso exibe uma informação no terminal
 
 '''
     print('Linha 1')
@@ -9,5 +9,12 @@
     print('Linha 5') # Imprime linha 5
 '''
 
-#print('Olá mundo!') #exibir ola mundo
+# print('Olá mundo!') # exibir ola mundo
 
+print('foi printar isso com \'aspas\'') # quando quiser escapar um caracter no print utiliza-se a contra barra.
+print('Tabém da pra utilizar "assim"') # usei um misto de aspas simple e aspas duplas.
+print('\n')
+print('A soma de 5+5 é:',5+5) # Exibe: 10
+print('Números tratados como texto: ', '5'+'5') # Exibe: 55
+print(-5.55*2) # Exibe: -11.1
+print('-5.55'*2) # -5.55-5.55
