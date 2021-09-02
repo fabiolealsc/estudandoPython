6e0561f6c07adf19c23c08c65e4681fd64f55698
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-02 01:52:53 -0300

    aulasBásicas
    É básico, mas importante.

---
+++ aula01/aula01.py
@@ -0,0 +1,8 @@
+print("Hello, world")
+
+def func():
+    pass
+
+while True:
+    txt = input('Digite um valor: ')
+    print(eval(txt))
---
+++ aula02/aula02.py
@@ -0,0 +1,13 @@
+# Esse é o modo de criação de comentários no python
+#print(123456) # Isso exibe uma informação no terminal
+
+'''
+    print('Linha 1')
+    print('Linha 2')
+    #print('Linha 3')
+    print('Linha 4')
+    print('Linha 5') # Imprime linha 5
+'''
+
+#print('Olá mundo!') #exibir ola mundo
+
